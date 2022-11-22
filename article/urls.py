36fd06d78e9a206f46370de88c85a4680d82f369
rex <905578725@qq.com>
2022-11-22 23:30:00 +0800

    文章增删改查接口

--- article/urls.py
+++ article/urls.py
@@ -18,5 +18,6 @@ from article import views
 urlpatterns = [
     path('',views.article_list,name='article_list'),
     path('create',views.article_create,name='article_create'),
-    path('delete/<int:id>/',views.article_delete,name='article_delete')
+    path('delete/<int:id>/',views.article_delete,name='article_delete'),
+    path('update/<int:id>/',views.article_update,name='article_update')
 ]
--- article/views.py
+++ article/views.py
@@ -17,9 +17,7 @@ def article_create(request):
     if request.method == "POST":
         #将表单中的数据赋值到表单实例中
         article_form=ArticleForm(data=request.POST)
-        print(request.POST)
         #判断提交的数据是否满足模型的要求
-        print(article_form.is_valid())
         if article_form.is_valid():
             #保存数据，但暂时不提交到数据库
             new_article=article_form.save(commit=False)
@@ -34,7 +32,7 @@ def article_create(request):
 
 
 def article_delete(request,id):
-    #添加文章
+    #删除文章
     if request.method == "POST":
         #根据删除的id得到文章对象
         article_obj = Article.objects.get(id=id)
@@ -42,3 +40,15 @@ def article_delete(request,id):
         article_obj.delete=True
         article_obj.save()
         return HttpResponse("文章删除成功。")
+
+def article_update(request,id):
+    if request.method == "POST":
+        article_form = ArticleForm(data=request.POST)
+        if article_form.is_valid():
+            article_obj=Article.objects.get(id=id)
+            article_obj.title=request.POST['title']
+            article_obj.body=request.POST['body']
+            article_obj.save()
+            return HttpResponse("文章更新成功。")
+        else:
+            return HttpResponse("文章更新失败，内容错误。")
